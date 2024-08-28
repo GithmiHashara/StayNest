4de10c8c4e7c17c57ac0b40ec5d2f1b87a499aa1
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-08-29 02:40:32 +0530

    Add Search component to Navbar

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,19 +1,20 @@
 'use client';
 
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation"; // Update the import
 
 const Logo = () => {
     const router = useRouter();
+
     return (
-            <Image
+        <Image
             alt="Logo"
             src="/images/logo.png"
-            width={100}
-            height={100}
+            width={150}
+            height={150}
             className="hidden md:block cursor-pointer"
-            />
-        
+            onClick={() => router.push('/')} // Example of using the router to navigate
+        />
     );
 }
 
