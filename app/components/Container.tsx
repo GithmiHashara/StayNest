d00e93fccc4bc262c8ead220e751353bc6bfd55d
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-08-28 22:14:44 +0530

    Add Container component to Navbar

---
+++ app/components/Container.tsx
@@ -0,0 +1,7 @@
+const Container =() =>{
+    return (
+<div>Container</div>
+   )
+}
+
+export default Container;
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,19 @@
+import Container from "../Container";
+
 const Navbar = () => {
     return (
-       <div className="fixed w-full bg-white z-10 shadow-sm"> I am a nevbar</div> 
+       <div className="fixed w-full bg-white z-10 shadow-sm"> 
+       <div
+       className="
+       py-4
+       border-b-[1px]
+       "
+      >
+<Container>
+    
+</Container>
+       </div>
+    </div> 
     );
 }
 export default Navbar;
