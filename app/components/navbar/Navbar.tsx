26a076c52bf53b7dee545f047bde8c3fabdf8002
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-08-28 20:40:28 +0530

    Add navbar and container.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-       <div> I am a nevbar</div> 
+       <div className="fixed w-full bg-white z-10 shadow-sm"> I am a nevbar</div> 
     );
 }
 export default Navbar;
