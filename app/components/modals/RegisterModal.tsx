62666d5daf9de54803ced737c0de33b237c5b7d6
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-02 00:39:10 +0530

    Add RegisterModal component and useRegisterModal hook

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,11 +1,61 @@
+'use client'
 
+import axios from 'axios'
+import { useState, useCallback } from 'react'
+import { AiFillGithub } from 'react-icons/ai'
+import { FcGoogle } from 'react-icons/fc'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+
+import useRegisterModal from '@/app/hooks/useRegisterModal'
+
+import Button from '../Button'
+import Modal from './Modal'
 
 const RegisterModal = () => {
-   
+    const registerModal = useRegisterModal()
+
+    const [isLoading, setIsLoading] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    }
+  })
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true)
+
+    axios
+      .post('/api/register', data)
+      .then(() => {
+         registerModal.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+    }
+
     return (
-        <div>
-           
-        </div>
+        <Modal
+        disabled={isLoading}
+        // isOpen={registerModal.isOpen} disabled={isLoading}
+        isOpen={registerModal.isOpen}
+        title="Register"
+        actionLabel="Continue"
+        onClose={registerModal.onClose}
+        onSubmit={handleSubmit(onSubmit)}
+        // body={bodyContent}
+        // footer={footerContent}
+        />
     );
 };
 
