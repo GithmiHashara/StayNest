3cf9756a293493dc3b7f2bf0d4eb11234d189ccb
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-02 21:33:29 +0530

    Add ToasterProvider component
    The ToasterProvider component is added to provide a toast notification system using the react-hot-toast library.

---
+++ app/providers/ToasterProvider.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { Toaster } from "react-hot-toast";
+
+const ToasterProvider = () => {
+    return (
+        <Toaster
+            position="top-right"
+            reverseOrder={false}
+        />
+    );
+}
+
+export default ToasterProvider;
