0c599d3b7553a35df17bd947347f74ad6eacfa72
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-05 23:15:02 +0530

    Update Avatar component to use lazy loading for images
    The Avatar component in the Avatar.tsx file is updated to use lazy loading for images. This change improves the page load performance by loading images only when they are visible on the screen.

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,22 +1,21 @@
-'use client ';
+'use client'
 
-import Image from 'next/image';
+import Image from 'next/image'
 
-// interface AvatarProps {
-//   src: string | null | undefined
-// }
+interface AvatarProps {
+  src: string | null | undefined
+}
 
-//const Avatar: React.FC<AvatarProps> = ({ src }) => {
-    const Avatar = () => {
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
     <Image
       className="rounded-full"
       height="30"
       width="30"
       alt="Avatar"
-      src= '/images/placeholder.jpg'
+      src={src || '/images/placeholder.jpg'}
     />
   )
 }
 
-export default Avatar;
+export default Avatar
