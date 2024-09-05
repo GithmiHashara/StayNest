c29311c663593629ffc1c78509e20c4985bb2d17
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-05 23:55:26 +0530

    Add Categories component to Navbar
    The Navbar component in the Navbar.tsx file is updated to include the Categories component. This change enhances the navigation menu by adding a new section for browsing different categories.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Search from "./Search";
 import UserMenu from "./UserMenu";
 import React from "react";
 import { SafeUser } from "@/app/types";
+import Categories from "./Categories";
 
 interface NavbarProps {
     currentUser?: SafeUser | null;
@@ -42,6 +43,7 @@ const Navbar: React.FC<NavbarProps> = ({
     </div>
 </Container>
       </div>
+      <Categories />
     </div> 
     );
 }
