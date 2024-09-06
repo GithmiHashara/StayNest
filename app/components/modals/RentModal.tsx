deeb56b8e1a46ed745b8870dbf1a1729531b92f1
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-07 01:04:32 +0530

    feat: Add dynamic loading for Map component in RentModal
    The RentModal component is updated to use dynamic loading for the Map component. This ensures that the Map component is only loaded when the location changes, improving performance. Additionally, the RentModal component is updated to include a location selection step, allowing users to specify the location of their place using a CountrySelect component. This enhancement improves the user experience by providing a convenient way to input the location information.

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -2,14 +2,16 @@
 
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
-import { useMemo, useState } from "react";
+import { use, useMemo, useState } from "react";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import { it } from "node:test";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
-import Map from "../Map";
+import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
+
 
 enum STEPS { // Enumerated type mean that we can only choose from a fixed set of values to assign to a variable 
     
@@ -51,6 +53,18 @@ const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location'); // watch is a hook that allows us to watch the value of a field in the form
+  const guestCount = watch('guestCount');
+  const roomCount = watch('roomCount');
+  const bathroomCount = watch('bathroomCount');
+ 
+  // we are using useMemo to make sure that the map component is only loaded when the location changes
+  const Map = useMemo(
+    () =>
+      dynamic(() => import('../Map'), {
+        ssr: false
+      }),
+    [location] // eslint-disable-line react-hooks/exhaustive-deps
+  )
  
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -121,7 +135,40 @@ const RentModal = () => {
           onChange={(value) => setCustomValue('location', value)}
         />
         {/* <Map center={location?.latlng} /> */}
-        <Map />
+        <Map 
+        center={location?.latlng}
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Tell us more about your place"
+          subtitle="Help guests know what to expect" //What amenities do you have?
+        />
+        <Counter
+          title="Guests"
+          subtitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomValue('guestCount', value)} //pass the value to the setCustomValue function
+        />
+        <hr />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          value={roomCount}
+          onChange={(value) => setCustomValue('roomCount', value)}
+        />
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+          value={bathroomCount}
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
       </div>
     )
   }
