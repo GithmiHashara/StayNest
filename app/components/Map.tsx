1485919ad435a76a0178f407fb3594d14c75be70
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-06 23:58:54 +0530

    Add Map component

---
+++ app/components/Map.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+const Map= () => {
+    // Your component logic here
+
+    return (
+        <div>
+            {/* Your JSX here */}
+        </div>
+    );
+};
+
+export default Map;
