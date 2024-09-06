891a5348ca745883456df9fe9af217ae8e842949
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-06 19:22:23 +0530

    Improve modal styling and layout
    The Modal component in Modal.tsx is updated to improve its styling and layout. The changes include modifying the background color and adding additional flex properties to center the content. This enhancement enhances the visual appearance and user experience of the modal.

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -59,7 +59,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, onSubmit, title, body, f
 
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70 ">
         <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
           {/* CONTENT */}
           <div
