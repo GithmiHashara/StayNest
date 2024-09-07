3d247c980e55263a3192860e2db23f4d5e482789
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-07 22:16:25 +0530

    Add getListings and getCurrentUser functions to fetch listings and current user data

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,17 @@
 import { TbCoinRupee } from 'react-icons/tb';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
+import getListings from './actions/getListings';
+import ListingCard from './components/listings/ListingCard';
+import getCurrentUser from './actions/getCurrentUser';
 
 
-export default function Home() {
-  const isEmpty = true;
-  if(isEmpty) {
+export default async function Home() {
+  const listings = await getListings() ;
+  const currentUser = await getCurrentUser();
+ 
+
+  if(listings.length === 0) {
     return (
       <EmptyState showReset={true} />
     )
@@ -15,10 +21,14 @@ export default function Home() {
   
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-       <div>
-        My Future Listings
-       </div>
-      </div>
+      {listings.map((listing) => (
+            <ListingCard
+              key={listing.id}
+              data={listing}
+              currentUser={currentUser}
+            />
+          ))}
+        </div>
     </Container>
    
   )
