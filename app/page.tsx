ee5b696d58cf667befa98069c0cde3fce35afa71
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-07 20:58:48 +0530

    Home component to display future listings

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 
+import { TbCoinRupee } from 'react-icons/tb';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+
+
 export default function Home() {
+  const isEmpty = true;
+  if(isEmpty) {
+    return (
+      <EmptyState showReset={true} />
+    )
+  }
   return (
-    <div className="text-rose-950 text-2xl">
-      Hello StayNest 
-    </div>
+  
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+       <div>
+        My Future Listings
+       </div>
+      </div>
+    </Container>
+   
   )
 }
