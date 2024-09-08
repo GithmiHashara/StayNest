db9c7683922045b76791ca6837b74d06d3c56af4
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-08 20:36:00 +0530

    Refactor ListingClient component to import dependencies and update structure

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,5 +1,9 @@
+import Container from "@/app/components/Container";
+import ListingHead from "@/app/components/listings/ListingHead";
+import { categories } from "@/app/components/navbar/Categories";
 import { SafeListing, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
+import { useMemo } from "react";
 
 interface ListingClientProps {
     reservations?: Reservation[];
@@ -13,14 +17,28 @@ interface ListingClientProps {
       listing, 
       currentUser
      }) => {
-
-
-
+   
+        const category = useMemo(() => {
+            return categories.find((item) => item.label === listing.category);
+          }, [listing.category])
+        
     return (
-        <div>
-           Listing Client
-        </div>
+       <Container>
+        <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+          </div>
+          </div>
+       </Container>
+       
     )
+    
 };
 
 export default ListingClient;
