03d176f3d003484caf0b7dcf5fa693be9c10415a
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-08 23:16:55 +0530

    Refactor ListingHead component to import dependencies, update structure, and add image and heart button functionality

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
+'use client'
 
-const ListingHead: React.FC = () => {
-    return (
-        <div>
-            {/* Your component code here */}
+import Image from 'next/image'
+
+import { SafeUser } from '@/app/types'
+import useCountries from '@/app/hooks/useCountries'
+import Heading from '../Heading'
+import HeartButton from '../HeartButton'
+
+interface ListingHeadProps {
+  title: string
+  imageSrc: string
+  locationValue: string
+  id: string
+  currentUser?: SafeUser | null
+}
+
+const ListingHead: React.FC<ListingHeadProps> = ({ title, imageSrc, locationValue, id, currentUser }) => {
+  const { getByValue } = useCountries();
+
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+      />
+      <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
+        <Image
+          alt="Image"
+          src={imageSrc}
+          fill
+          className="object-cover w-full"
+        />
+        <div className="absolute top-5 right-5">
+          <HeartButton
+            listingId={id}
+            currentUser={currentUser}
+          />
         </div>
-    );
-};
+      </div>
+    </>
+  )
+}
 
-export default ListingHead;
+export default ListingHead
