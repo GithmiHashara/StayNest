73e937814b46840b4b5c0d806f9445f76d18443b
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-09 21:46:59 +0530

    Refactor ListingClient component to add useLoginModal hook and useRouter hook

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -4,8 +4,10 @@ import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
 import ListingInfo from "@/app/components/listings/ListingInfo";
 import { categories } from "@/app/components/navbar/Categories";
+import useLoginModal from "@/app/hooks/useLoginModal";
 import { SafeListing, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
+import { useRouter } from "next/navigation";
 import { useMemo } from "react";
 
 const initialDateRange = {
@@ -29,7 +31,12 @@ interface ListingClientProps {
      }) => {
 
       //login model
-      
+      const loginModal = useLoginModal();
+      const router = useRouter();
+
+      const disabledDates = useMemo(() => {
+
+      }, []);
    
         const category = useMemo(() => {
             return categories.find((item) => item.label === listing.category);
