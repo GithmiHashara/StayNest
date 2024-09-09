c9cc49f3496470f5d4d7a99ab49919155701a592
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-09 19:19:16 +0530

    Refactor ListingClient component to update structure and add ListingInfo component

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -35,6 +35,17 @@ interface ListingClientProps {
             id={listing.id}
             currentUser={currentUser}
           />
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <ListingInfo //a component
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+            </div>
           </div>
           </div>
        </Container>
