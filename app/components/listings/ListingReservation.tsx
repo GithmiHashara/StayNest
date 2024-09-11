99346d2823d63811455c04779b1803338bb842f6
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-11 14:18:28 +0530

    Add ListingReservation component

---
+++ app/components/listings/ListingReservation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListingReservation: React.FC = () => {
+    return (
+        <div>
+            {/* Your component JSX goes here */}
+        </div>
+    );
+};
+
+export default ListingReservation;
