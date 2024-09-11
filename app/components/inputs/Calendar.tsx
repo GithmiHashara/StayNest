6574cd4a471da61a1fff00e3d5301829255325b0
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-11 15:05:46 +0530

    Refactor Calendar component to use react-date-range library

--- app/components/inputs/Calendar.tsx
+++ app/components/inputs/Calendar.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+'use client'
 
-const Calendar: React.FC = () => {
-    return (
-        <div>
-            {/* Your calendar component goes here */}
-        </div>
-    );
-};
+import { DateRange, Range, RangeKeyDict } from 'react-date-range'
 
-export default Calendar;
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
+
+interface CalendarProps {
+  value: Range // Range is an object with a key of type RangeKey and a value of type Date
+  onChange: (value: RangeKeyDict) => void // RangeKeyDict is an object with a key of type RangeKey and a value of type Range
+  disabledDates?: Date[] 
+}
+
+const Calendar: React.FC<CalendarProps> = ({ value, onChange, disabledDates }) => {
+  return (
+    <DateRange
+      rangeColors={['#852541']} //#262626
+      ranges={[value]}
+      date={new Date()}
+      onChange={onChange}
+      direction="vertical"
+      showDateDisplay={false}
+      minDate={new Date()}
+      disabledDates={disabledDates}
+    />
+  )
+}
+
+export default Calendar
