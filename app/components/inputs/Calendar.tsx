c6b1fd580d4c931cd4fa1950bdf6acd4545a332a
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-11 14:28:05 +0530

    Add Calendar component

---
+++ app/components/inputs/Calendar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calendar: React.FC = () => {
+    return (
+        <div>
+            {/* Your calendar component goes here */}
+        </div>
+    );
+};
+
+export default Calendar;
