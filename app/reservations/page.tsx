a1276c6987b637b98c34f94c5a27353548c0bdcc
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-12 16:19:05 +0530

    Add ReservationsPage component

---
+++ app/reservations/page.tsx
@@ -0,0 +1,41 @@
+import EmptyState from "../components/EmptyState";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+import ReservationsClient from "./ReservationsClient ";
+
+const ReservationsPage  = async () => {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser)
+         return (
+    <EmptyState
+     title="Unauthorized" 
+     subtitle="Please login"
+     />
+
+         );
+         const reservations = await getReservations({
+            authorId: currentUser.id
+         });
+
+         if (!reservations.length) {
+             return (
+                 <EmptyState
+                 title="No reservations"
+                 subtitle="You don't have any reservations yet"
+                 />
+             );
+         }
+
+         return(
+            <ReservationsClient 
+            reservations={reservations}
+            currentUser={currentUser}
+            />
+         )
+}
+
+export default ReservationsPage;
+
+
