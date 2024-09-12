68b0f4a9f22d80bfc74db01fc91ef9b2baa6d13d
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-12 16:19:10 +0530

    Add ReservationsClient component

---
+++ app/reservations/ReservationsClient .tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ReservationsClient: React.FC = () => {
+    // Your component logic here
+
+    return (
+        <div>
+            {/* Your JSX here */}
+        </div>
+    );
+};
+
+export default ReservationsClient;
