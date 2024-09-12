55b44d73350f1971f002056c520790d21b1500ca
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-13 00:13:06 +0530

    Refactor FavoritesClient component to use "Favourites" instead of "Favorites" in the title and subtitle

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -15,8 +15,8 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({ listings, currentUser
   return (
     <Container>
       <Heading
-        title="Favorites"
-        subtitle="List of places you have favorited!"
+        title="Favourites"
+        subtitle="List of places you have favourited!"
       />
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {listings.map((listing) => (
