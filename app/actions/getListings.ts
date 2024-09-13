397201862e5dceaaa64ec078bd29570e5dcc5272
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-13 16:48:27 +0530

    Refactor getListings function to include additional search parameters
    This commit refactors the getListings function in the app/actions/getListings.ts file to include additional search parameters for filtering listings. The new parameters include guestCount, roomCount, bathroomCount, locationValue, startDate, endDate, and category. The function now builds a query object based on these parameters and uses it to fetch listings from the database. The fetched listings are then transformed to include the createdAt property in ISO string format. This refactor improves the search functionality of the application.

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,36 +2,84 @@ import prisma from '@/app/libs/prismadb'
 
 export interface IListingsParams {
   userId? : string;
+  guestCount? : number;
+  roomCount? : number;
+  bathroomCount? : number;
+  locationValue? : string;
+  startDate? : string;
+  endDate? : string;
+  category? : string;
+}
+export default async function getListings(params: IListingsParams) {
+  try {
+    const { userId, roomCount, guestCount, bathroomCount, locationValue, startDate, endDate, category } = params
+
+    let query: any = {}
+
+    if (userId) {
+      query.userId = userId
+    }
+
+    if (category) {
+      query.category = category
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount
+      }
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount // gte = greater than or equal
+      }
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount
+      }
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue
+    }
+
+    if (startDate && endDate) {
+      query.NOT = { // NOT = not in the following list (reverse filtering)
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate },
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate }
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    const listings = await prisma.listing.findMany({
+      where: query,
+      orderBy: {
+        createdAt: 'desc'
+      }
+    })
+
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString()
+    }))
+
+    return safeListings
+  } catch (error: any) {
+    throw new Error(error)
+  }
 }
 
-export default async function getListings(
-  params: IListingsParams
-){
-    try {
-      const { userId } = params;
-
-      let query: any = {};
-
-      if (userId) {
-        query.userId = userId;
-      }
-
-
-        const listings = await prisma.listing.findMany({
-            where : query,
-            orderBy: {
-              createdAt: 'desc'
-            }
-          });
-
-          const safeListings = listings.map((listing) => ({
-            ...listing,
-            createdAt: listing.createdAt.toISOString()
-          }));
-
-          return safeListings;
-
-    } catch (error : any) {
-        throw new Error(error);
-    }
-    }
