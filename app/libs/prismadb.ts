4a959a3ae43775ecd0005ab54697b95319e7077c
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-14 00:36:44 +0530

    Refactor PrismaClient initialization to enable logging

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -5,11 +5,12 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-
-// Create a new PrismaClient instance if there isn't one already
-const client = globalThis.prisma || new PrismaClient()
+// Create a new PrismaClient instance with logging enabled
+const client = globalThis.prisma || new PrismaClient({
+  log: ['query', 'info', 'warn', 'error']  // Enables Prisma logs for better debugging
+})
 
 // Assign the prisma client to the global object if it doesn't exist
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = client
 
-export default client;
+export default client
