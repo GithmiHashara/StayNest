24da33ee4fd858096be32362927458fe3135d93d
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-13 17:48:11 +0530

    Refactor Search component to display selected search parameters in the navbar

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,10 +1,56 @@
 'use client'
 
+import useCountries from "@/app/hooks/useCountries";
 import useSearchModal from "@/app/hooks/useSearchModal";
+import { differenceInDays } from "date-fns";
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 
 const Search = () => {
   const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const {getByValue} = useCountries();
+
+  const locationValue = params?.get('locationValue')
+  const startDate = params?.get('startDate')
+  const endDate = params?.get('endDate')
+  const guestCount = params?.get('guestCount')
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label
+    }
+
+    return 'Anywhere'
+  }, [getByValue, locationValue])
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string)
+      const end = new Date(endDate as string)
+
+      let diff = differenceInDays(end, start)
+
+      if (diff === 0) {
+        diff = 1
+      }
+
+      return `${diff} Days`
+    }
+
+    return 'Any Week'
+  }, [startDate, endDate]);
+
+  //guest lable
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return 'Add Guests';
+  }, [guestCount]);
 
     return (
     <div
@@ -22,7 +68,7 @@ const Search = () => {
          "
           >
             <div className="text-sm font-semibold px-6"  >
-              Anywhere
+              {locationLabel}
             </div>
             <div
             className="
@@ -36,7 +82,7 @@ const Search = () => {
             text-center
           "
             > 
-            Any Week
+           {durationLabel}
             </div>
             <div
             className="
@@ -51,7 +97,7 @@ const Search = () => {
           "
             >
               <div className="hidden sm:block ">
-              Add Gusts
+              {guestLabel}
               </div>
               <div className="
               p-2
