e46e26ffb3fe75b611a2383d60517d7a9c7ace6b
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-22 19:26:06 +0530

    Refactor Home component to include listing details in SafeListing interface

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import getCurrentUser from './actions/getCurrentUser';
 // types.ts
 
 export interface SafeListing {
+  listing:any,
   id: string;
   title: string;
   description: string;
@@ -32,7 +33,7 @@ const Home = async ({ searchParams }: HomeProps) => {
   return (
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {listings.map((listing: Listing) => (
+        {listings.map((listing) => (
           <ListingCard
             key={listing.id}
             data={listing}
