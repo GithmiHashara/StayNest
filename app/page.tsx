91894faad73d8b1955ae001791d9c0e9bb9e6a36
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-22 19:19:50 +0530

    Refactor Home component to use Prisma model and add type annotations

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import { TbCoinRupee } from 'react-icons/tb';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
@@ -6,39 +5,43 @@ import getListings, { IListingsParams } from './actions/getListings';
 import ListingCard from './components/listings/ListingCard';
 import getCurrentUser from './actions/getCurrentUser';
 
-interface HomeProps {
-  searchParams: IListingsParams
+// types.ts
+
+export interface SafeListing {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  locationValue: string;
+  // Add all other fields that 'listing' might contain
+  createdAt: string; // Make sure to convert dates to string as you're doing in getListings
 }
 
+interface HomeProps {
+  searchParams: IListingsParams;
+}
 
-const Home = async ({searchParams}: HomeProps)=> {
-  const listings = await getListings(searchParams) ;
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
- 
 
-  if(listings.length === 0) {
-    return (
-      <EmptyState showReset={true} />
-    )
+  if (listings.length === 0) {
+    return <EmptyState showReset={true} />;
   }
 
-
-
   return (
-  
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-      {listings.map((listing) => (
-            <ListingCard
-              key={listing.id}              
-              data={listing}
-              currentUser={currentUser}
-            />
-          ))}
-        </div>
+        {listings.map((listing: Listing) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
     </Container>
-   
-  )
-}
+  );
+};
 
 export default Home;
