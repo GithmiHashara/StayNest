c7d2457455fad7342dd59467cc24aa7ea16393a6
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-22 19:30:45 +0530

    Refactor Home component to remove commented out code and unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,50 @@
+
+// import { TbCoinRupee } from 'react-icons/tb';
+// import Container from './components/Container';
+// import EmptyState from './components/EmptyState';
+// import getListings, { IListingsParams } from './actions/getListings';
+// import ListingCard from './components/listings/ListingCard';
+// import getCurrentUser from './actions/getCurrentUser';
+
+// interface HomeProps {
+//   searchParams: IListingsParams
+// }
+
+
+// const Home = async ({searchParams}: HomeProps)=> {
+//   const listings = await getListings(searchParams) ;
+//   const currentUser = await getCurrentUser();
+ 
+
+//   if(listings.length === 0) {
+//     return (
+//       <EmptyState showReset={true} />
+//     )
+//   }
+
+
+
+//   return (
+  
+//     <Container>
+//       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+//       {listings.map((listing) => (
+//             <ListingCard
+//               key={listing.id}              
+//               data={listing}
+//               currentUser={currentUser}
+//             />
+//           ))}
+//         </div>
+//     </Container>
+   
+//   )
+// }
+
+// export default Home;
+
+
+
 import { TbCoinRupee } from 'react-icons/tb';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
